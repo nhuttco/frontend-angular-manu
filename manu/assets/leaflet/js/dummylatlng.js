cef5220a43091b894b158d8d4be29bb5ed96b131
nhut <nhuttco@gmail.com>
2018-10-12 11:19:16 +0700

    Updates

---
+++ manu/assets/leaflet/js/dummylatlng.js
@@ -0,0 +1,35 @@
+
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+var infoData = "<div class=\"cplgr-listing-grid\">\n  <div class=\"cplgr-listing-thumb-container\">\n    <div class=\"cplgr-listing-thumb\">\n      <a href=\"#\">\n        <img src=\"images/listing/listing-thumb-1.jpg\" class=\"img-responsive\" alt=\"\">\n      </a>\n    </div>\n    <div class=\"cplgr-listing-quick-action\">\n      <ul>\n        <li class=\"ratings\">\n          <i class=\"fa fa-star\"></i>\n          <i class=\"fa fa-star\"></i>\n          <i class=\"fa fa-star\"></i>\n          <i class=\"fa fa-star\"></i>\n          <i class=\"fa fa-star\"></i>\n        </li>\n        <li class=\"bookmark\">\n          <a href=\"#\">\n            <i class=\"fa fa-bookmark\"></i>\n          </a>\n        </li>\n        <li class=\"preview\">\n          <a href=\"#myMapModal_1\" class=\"btn\" data-toggle=\"modal\"><i class=\"fa fa-eye\"></i></a>\n        </li>\n      </ul>\n    </div>\n    <div class=\"cplgr-listing-grid-hover\">\n      <div class=\"price-range\">\n        <p>$150-$600</p>\n      </div>\n    </div>\n  </div>\n  <div class=\"cplgr-listing-description-container\">\n    <div class=\"cplgr-listing-description\">\n      <div class=\"author-avatar-thumb\">\n        <a href=\"#\">\n          <img src=\"images/author/author-thumb-1.jpg\" class=\"img-responsive\" alt=\"\">\n        </a>\n        <div class=\"cplgr-verified\" title=\"verified\"></div>\n      </div>\n      <div class=\"cplgr-listing-categories\">\n        <a href=\"#\"><i class=\"fa fa-home\" aria-hidden=\"true\"></i> Real Estate</a>\n      </div>\n      <div class=\"cplgr-status-open\">\n        <p>Open</p>\n      </div>\n      <div class=\"fix\"></div>\n      <h6 class=\"cplgr-listing-title\">\n        <a href=\"#\">California housing</a>\n      </h6>\n      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>\n    </div>\n    <div class=\"cplgr-listing-desription-footer\">\n      <a href=\"#\" class=\"cplgr-listing-location\"><i class=\"fa fa-map-marker\"></i> California</a>\n      <div class=\"cplgr-phone-number\"><i class=\"fa fa-phone\"></i> +1-0000-000-000</div>\n    </div>\n  </div>\n</div>";
+var addressPoints = [
+[-37.8210922667, 175.2209316333, infoData],
+[-37.8210819833, 175.2213903167, infoData],
+[-37.8210881833, 175.2215004833, infoData],
+[-37.8211946833, 175.2213655333, infoData],
+[-37.8209458667, 175.2214051333, infoData],
+[-37.8208292333, 175.2214374833, infoData],
+[-37.8325816, 175.2238798667, infoData],
+[-37.8315855167, 175.2279767, infoData],
+[-37.8096336833, 175.2223743833, infoData],
+[-37.80970685, 175.2221815833, infoData],
+[-37.8102146667, 175.2211562833, infoData],
+[-37.8088037167, 175.2242227, infoData],
+[-37.8112330167, 175.2193425667, infoData],
+[-37.8116368667, 175.2193005167, infoData],
+[-37.80812645, 175.2255449333, infoData],
+[-37.8080231333, 175.2286383167, infoData],
+[-37.8089538667, 175.2222222333, infoData],
+[-37.8080905833, 175.2275400667, infoData],
+[-37.808811, 175.2227592833, infoData],
+[-37.80832975, 175.2276898167, infoData],
+[-37.8089395333, 175.2281710333, infoData],
+[-37.8093421, 175.2274883167, infoData],
+[-37.8084820833, 175.22601925, infoData],
+[-37.80881015, 175.22622865, infoData],
+[-37.8090947667, 175.2263585667, infoData],
+[-37.8092962333, 175.2244872333, infoData],
+[-37.8091016667, 175.2249140167, infoData],
+[-37.8088785167, 175.2253611667, infoData],
+[-37.80825965, 175.22530115, infoData]
+];
